91f5b4992d78f0919115b852bd3bbd6ffb865304
AdnanJ <adnan.webdev@gmail.com>
2021-01-29 02:58:14 +1100

    Add project files.

---
+++ netcore/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netcore.Data;
+using netcore.Models;
+
+namespace netcore.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController (ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.MyProperty;
+            return View(objList);
+        }
+    }
+}
---
+++ netcore/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using netcore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Category> Category { get; set; }
+
+        public DbSet<Category> MyProperty { get; set; }
+        public IEnumerable<Category> Category { get; internal set; }
+    }
+}
